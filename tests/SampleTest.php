4253d1fdb329b1713b42201c93d383dca9217877
kkznch <kkznch710md@gmail.com>
2020-12-12 15:59:31 +0900

    [add]サンプル用のテストファイルを作成した

---
+++ tests/SampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SampleTest
+ */
+class SampleTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testFirst()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     *
+     */
+    public function testSecond()
+    {
+        $this->assertTrue(true);
+    }
+}
